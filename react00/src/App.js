5ecff9974ec32fe71b1a1930cc60015a5c0471fa
최보민 <bomin1995@naver.com>
2022-07-25 14:10:17 +0900

    20220725

---
+++ react00/src/App.js
@@ -0,0 +1,19 @@
+import Test from "./test";
+import Nav from "./Nav";
+
+const App = () => {
+  //console.log('하하하')
+  return (
+    <>
+      <Test />
+      <h1>호에엥</h1>
+      <Test />
+      <Nav />
+      <Test />
+
+      {/* <h2>호에엥</h2> 주석은 컨트롤+/ */}
+    </>
+  )
+}
+
+export default App;
---
+++ react00/src/test.js
@@ -0,0 +1,11 @@
+// const Test = () => {}
+
+function Test() {
+    return (
+        <>
+            <i className="xi-bars"></i>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </>
+    )
+}
+export default Test;
